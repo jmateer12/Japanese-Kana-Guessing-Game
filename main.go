4bf24ffeca63b91680cb9eede58384920e1e4d40
jmateer12 <jmateer@liberty.edu>
2026-01-23 23:06:28 -0500

    Created a function to switch between the two different kana types

--- main.go
+++ main.go
@@ -2,128 +2,141 @@ package main
 
 import "fmt"
 
-func main() {
-	/*
-		hiragana := map[string]string{
-			"あ": "a",
-			"い": "i",
-			"う": "u",
-			"え": "e",
-			"お": "o",
-			"か": "ka",
-			"き": "ki",
-			"く": "ku",
-			"け": "ke",
-			"こ": "ko",
-			"さ": "sa",
-			"し": "shi",
-			"す": "su",
-			"せ": "se",
-			"そ": "so",
-			"た": "ta",
-			"ち": "chi",
-			"つ": "tsu",
-			"て": "te",
-			"と": "to",
-			"な": "na",
-			"に": "ni",
-			"ぬ": "nu",
-			"ね": "ne",
-			"の": "no",
-			"は": "ha",
-			"ひ": "hi",
-			"ふ": "fu",
-			"へ": "he",
-			"ほ": "ho",
-			"ま": "ma",
-			"み": "mi",
-			"む": "mu",
-			"め": "me",
-			"も": "mo",
-			"や": "ya",
-			"ゆ": "yu",
-			"よ": "yo",
-			"ら": "ra",
-			"り": "ri",
-			"る": "ru",
-			"れ": "re",
-			"ろ": "ro",
-			"わ": "wa",
-			"ゐ": "wi",
-			"を": "wo",
-			"ん": "n",
-		}
-	*/
+var hiragana = map[string]string{
+	"あ": "a",
+	"い": "i",
+	"う": "u",
+	"え": "e",
+	"お": "o",
+	"か": "ka",
+	"き": "ki",
+	"く": "ku",
+	"け": "ke",
+	"こ": "ko",
+	"さ": "sa",
+	"し": "shi",
+	"す": "su",
+	"せ": "se",
+	"そ": "so",
+	"た": "ta",
+	"ち": "chi",
+	"つ": "tsu",
+	"て": "te",
+	"と": "to",
+	"な": "na",
+	"に": "ni",
+	"ぬ": "nu",
+	"ね": "ne",
+	"の": "no",
+	"は": "ha",
+	"ひ": "hi",
+	"ふ": "fu",
+	"へ": "he",
+	"ほ": "ho",
+	"ま": "ma",
+	"み": "mi",
+	"む": "mu",
+	"め": "me",
+	"も": "mo",
+	"や": "ya",
+	"ゆ": "yu",
+	"よ": "yo",
+	"ら": "ra",
+	"り": "ri",
+	"る": "ru",
+	"れ": "re",
+	"ろ": "ro",
+	"わ": "wa",
+	"ゐ": "wi",
+	"を": "wo",
+	"ん": "n",
+}
 
-	katakana := map[string]string{
-		"ア": "a",
-		"イ": "i",
-		"ウ": "u",
-		"エ": "e",
-		"オ": "o",
-		"カ": "ka",
-		"キ": "ki",
-		"ク": "ku",
-		"ケ": "ke",
-		"コ": "ko",
-		"サ": "sa",
-		"シ": "shi",
-		"ス": "su",
-		"セ": "se",
-		"ソ": "so",
-		"タ": "ta",
-		"チ": "chi",
-		"ツ": "tsu",
-		"テ": "te",
-		"ト": "to",
-		"ナ": "na",
-		"ニ": "ni",
-		"ヌ": "nu",
-		"ネ": "ne",
-		"ノ": "no",
-		"ハ": "ha",
-		"ヒ": "hi",
-		"フ": "fu",
-		"ヘ": "he",
-		"ホ": "ho",
-		"マ": "ma",
-		"ミ": "mi",
-		"ム": "mu",
-		"メ": "me",
-		"モ": "mo",
-		"ヤ": "ya",
-		"ユ": "yu",
-		"ヨ": "yo",
-		"ラ": "ra",
-		"リ": "ri",
-		"ル": "ru",
-		"レ": "re",
-		"ロ": "ro",
-		"ワ": "wa",
-		"ヰ": "wi",
-		"ヲ": "wo",
-		"ン": "n",
+var katakana = map[string]string{
+	"ア": "a",
+	"イ": "i",
+	"ウ": "u",
+	"エ": "e",
+	"オ": "o",
+	"カ": "ka",
+	"キ": "ki",
+	"ク": "ku",
+	"ケ": "ke",
+	"コ": "ko",
+	"サ": "sa",
+	"シ": "shi",
+	"ス": "su",
+	"セ": "se",
+	"ソ": "so",
+	"タ": "ta",
+	"チ": "chi",
+	"ツ": "tsu",
+	"テ": "te",
+	"ト": "to",
+	"ナ": "na",
+	"ニ": "ni",
+	"ヌ": "nu",
+	"ネ": "ne",
+	"ノ": "no",
+	"ハ": "ha",
+	"ヒ": "hi",
+	"フ": "fu",
+	"ヘ": "he",
+	"ホ": "ho",
+	"マ": "ma",
+	"ミ": "mi",
+	"ム": "mu",
+	"メ": "me",
+	"モ": "mo",
+	"ヤ": "ya",
+	"ユ": "yu",
+	"ヨ": "yo",
+	"ラ": "ra",
+	"リ": "ri",
+	"ル": "ru",
+	"レ": "re",
+	"ロ": "ro",
+	"ワ": "wa",
+	"ヰ": "wi",
+	"ヲ": "wo",
+	"ン": "n",
+}
+
+func main() {
+
+	choice := ""
+	fmt.Printf("Enter 1 to test your hiragana and 2 to test your katakana: ")
+	fmt.Scanln(&choice)
+
+	if choice == "1" {
+		TestCharacters(hiragana)
 	}
 
+	if choice == "2" {
+		TestCharacters(katakana)
+	}
+
+}
+
+func TestCharacters(kana map[string]string) {
 	answersCorrect := 0
 
-	for character := range katakana {
+	for character := range kana {
 		guess := ""
 
 		fmt.Printf("What does this character represent %s: ", character)
 		fmt.Scanln(&guess)
 
-		if guess == katakana[character] {
+		if guess == kana[character] {
 			answersCorrect += 1
 			fmt.Printf("\033[32mAnswer Correct!!!\033[0m\n")
 		} else {
-			fmt.Printf("\033[31mAnswer Incorrect, answer should be %s\033[0m\n", katakana[character])
+			fmt.Printf("\033[31mAnswer Incorrect, answer should be %s\033[0m\n", kana[character])
 		}
 	}
 
-	percent := (float64(answersCorrect) / float64(len(katakana))) * 100
+	percent := (float64(answersCorrect) / float64(len(kana))) * 100
 
 	fmt.Printf("Answers correct: %d/%d\n", answersCorrect, len(katakana))
 	fmt.Printf("Percent correct: %.1f%%\n", percent)
-
 }
